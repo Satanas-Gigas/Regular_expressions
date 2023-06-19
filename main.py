71f1a02ad53bab6487c12776831f667ddeb48654
Satanas-Gigas <120686392+Satanas-Gigas@users.noreply.github.com>
2023-06-19 05:59:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,33 +2,61 @@
 import csv
 import re
 
+phone_number_pattern = re.compile(r"^(\+7|8)\D*(\d{3})\D*(\d{3})\D*(\d{2})\D*(\d{2})\D*(\d{4})?.*")
+def format_phone(phone):
+    numbers = phone_number_pattern.match(phone)
+    if numbers.groups()[5]:
+        phone = phone_number_pattern.sub(r"+7(\2)\3-\4-\5 доб.\6", phone)
+    else:
+        phone = phone_number_pattern.sub(r"+7(\2)\3-\4-\5", phone)
 
-with open("phonebook_raw.csv", encoding='utf-8') as f:
-    rows = csv.reader(f, delimiter=",")
-    contacts_list = list(rows)
-    contacts_list[4].pop(-1)
+    return phone
 
-pattern = r"^(\w+[À-ßà-ÿ])\W(\w+)\W(\w+)?\,+"
-substitution = r"\1,\2,\3,"
-for el in contacts_list[1:]:
-    text = ','.join(el[0:3])
-    text = re.sub(pattern, substitution, text).split(',')
-    el[0], el[1], el[2] = text[0], text[1], text[2]
+with open("phonebook_raw.csv", "r", encoding="utf8") as f:
+    raws_list = list(csv.reader(f, delimiter=","))
 
-pattern = r"(\+7|8)\s?\W?(\d{3})\W?\s?(\d{3})\W?(\d{2})\W?(\d{2})(\ )?\(?(\ ?доб.)? ?(\d{4})?\)?"
-substitution = r"+7(\2)\3-\4-\5\6\7\8"
-for element in contacts_list[1:]:
-    element[5] = re.sub(pattern, substitution, element[5])
+raws_info = {}
+for raw in raws_list[1:]:
+    lastname, firstname, surname, organization, position, phone, email = raw[:7]
+    name = re.findall(r"([(А-Я][а-я]+)", lastname+firstname+surname)
 
-for person in contacts_list[1:]:
-    for row in contacts_list[contacts_list.index(person) + 1:]:
-        if person[0] == row[0] and person[1] == row[1]:
-            for i, atribute in enumerate(person):
-                if not atribute:
-                    person[i] = row[i]
-            row[0] = 'del'
-contacts_list = [row for row in contacts_list if row[0] != 'del']
+    if len(name) < 2: continue
 
-with open("phonebook.csv", "w", encoding='utf-8', newline='') as f:
-    data_writer = csv.writer(f, delimiter=',')
-    data_writer.writerows(contacts_list)
+    raws_info.setdefault(name[0]+name[1], {})
+    raw_info = raws_info.get(name[0]+name[1])
+    print(name[0]+name[1])
+    print(raw_info)
+
+    raw_info["lastname"] = name[0]
+    raw_info["firstname"] = name[1]
+
+    # print(raw_info)
+    if len(name) > 2 and not raw_info.get("surname"):
+        raw_info["surname"] = name[2]
+
+    if organization and not raw_info.get("organization"):
+        raw_info["organization"] = organization
+
+    if position and not raw_info.get("position"):
+        raw_info["position"] = position
+
+    if phone and not raw_info.get("phone"):
+        raw_info["phone"] = format_phone(phone)
+
+    if email and not raw_info.get("email"):
+        raw_info["email"] = email
+
+raws_list = [raws_list[0]]
+for raw in raws_info.values():
+    raws_list.append([
+        raw.get("lastname"),
+        raw.get("firstname"),
+        raw.get("surname"),
+        raw.get("organization"),
+        raw.get("position"),
+        raw.get("phone"),
+        raw.get("email"),
+    ])
+
+with open("phonebook.csv", "w", encoding="utf8") as f:
+    csv.writer(f, delimiter=',').writerows(raws_list)
